7c21933cc94e6bb8ae41ab3895f22d119ca9868b
Graham Fairweather <xotic750@gmail.com>
2019-08-18 15:20:19 +0200

    :bookmark: v1.0.1

--- dist/util-get-getter-x.js
+++ dist/util-get-getter-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-18T13:17:32.144Z",
+  "date": "2019-08-18T13:20:03.536Z",
   "describe": "",
   "description": "Utility to get a getter.",
   "file": "util-get-getter-x.js",
