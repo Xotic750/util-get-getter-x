b994663b62ba1cd3267295b02d27061870272982
Graham Fairweather <xotic750@gmail.com>
2020-01-30 23:08:29 +0100

    Maintenance

--- dist/util-get-getter-x.js
+++ dist/util-get-getter-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-09-05T19:48:02.991Z",
+  "date": "2020-01-30T22:08:21.165Z",
   "describe": "",
   "description": "Utility to get a getter.",
   "file": "util-get-getter-x.js",
